55360ee40156b3ea004efa7247e31898f0f2cad3
Raj Zinzuvadiya-smartSense <raj.zinzuvadiya@smartsensesolutions.com>
2022-02-14 12:22:53 +0530

    Uncommented Diff.java

--- Diff.java
+++ Diff.java
@@ -1,8 +1,8 @@
 public class Diff {
     public static void main(String[] args) {
-        // int a = 5;
-        // int b = 6;
-        // System.out.println("The Diff is" + (a-b));
+         int a = 5;
+         int b = 6;
+         System.out.println("The Diff is" + (a-b));
     }
     
 }
